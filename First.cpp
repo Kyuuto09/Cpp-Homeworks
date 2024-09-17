9914e9d478923eaaf03e8b6963a616e651dc940b
showtyx <showtyx@DESKTOP-ERLTA8A>
2024-09-17 20:43:10 +0300

    Add project files.

---
+++ First.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main() {
+    // 1 -----
+    /*
+    cout << "J. Cole - No Role Modelz\n\n";
+    cout << "------------------------\n\n";
+    cout << "\"First things first, rest in peace Uncle Phil\n";
+    cout << "For real, you the only father that I ever knew\n";
+    cout << "I get my \\\' b\\\' back, I'mma be a better you\n";
+    cout << "Prophecies that I made way back in the Ville\"\n";
+    */
+
+    // 2 -----
+    /* 
+    cout << "Every hunter wants to know where the pheasant sits:\n";
+    cout << "Red\n";
+    cout << "\tOrange\n";
+    cout << "\t\tYellow\n";
+    cout << "\t\t\tGreen\n";
+    cout << "\t\t\t\tBlue\n";
+    cout << "\t\t\t\t\tIndigo\n";
+    cout << "\t\t\t\t\t\tViolet\n";
+    */
+
+    // 3 -----
+    cout << "Продам цуценят.\n";
+    cout << "Порода: ротвейлер.\n";
+    cout << "...\n";
+    cout << "Тел.: 222-22-22\n";
+    cout << "222    |    222\n";
+    cout << " 222  |  222\n";
+    cout << "    22 | 22\n";
+    cout << "    22 | 22\n";
+}
